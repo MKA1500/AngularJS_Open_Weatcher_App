9eb4d1f9915922ac09dd0d19d9334d270db7194d
MKA1500 <maciejkabsch@gmail.com>
2017-10-29 22:08:34 +0100

    weather data

--- js/app.js
+++ js/app.js
@@ -7,13 +7,26 @@ myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.myApiKey = "b43cf762e537bdce9ce724f6f27bb497";
 
-    $http.get("krakow_example.json").success(function(data){
+    $http.get("http://ip-api.com/json").success(function(data){
         $scope.position = data;
 
-        $scope.openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-            $scope.position.lat + "&lon=" + $scope.position.lon + "&appid=" + $scope.myApiKey;
+       var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+            $scope.position.lat + "&lon=" + $scope.position.lon + "&units=metric&appid=" + $scope.myApiKey;
 
-        console.log($scope.openWeatherURL);
+        console.log(openWeatherURL);
 
+        // https://openweathermap.org/current#current_JSON
+
+        // http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9a899e98ac276d14ee3ba6cd2a21598d
+
+        $http.get(openWeatherURL).success(function(data){
+            $scope.main = data.weather[0].description;
+            $scope.description = data.weather[0].description;
+            $scope.speed = data.wind.speed;
+            // m per s
+            $scope.temp = data.main.temp;
+            $scope.pressure = data.main.pressure;
+            $scope.humidity = data.main.humidity;
+        });
     });
 }]);
