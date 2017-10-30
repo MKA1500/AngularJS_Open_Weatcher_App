3094fe483f18c2751e8a38626d912179bbdb08b2
MKA1500 <maciejkabsch@gmail.com>
2017-10-30 09:22:18 +0100

    choosing new weather

--- js/app.js
+++ js/app.js
@@ -10,18 +10,14 @@ myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
     $http.get("http://ip-api.com/json").success(function(data){
         $scope.position = data;
 
-       var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+        $scope.openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" +
             $scope.position.lat + "&lon=" + $scope.position.lon + "&units=metric&appid=" + $scope.myApiKey;
-
-        console.log(openWeatherURL);
 
         // https://openweathermap.org/current#current_JSON
 
-        // http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9a899e98ac276d14ee3ba6cd2a21598d
-
         // https://openweathermap.org/weather-conditions
 
-        $http.get(openWeatherURL).success(function(data){
+        $http.get($scope.openWeatherURL).success(function(data){
             $scope.description = data.weather[0].description;
             $scope.speed = data.wind.speed;
             // m per s
@@ -30,6 +26,33 @@ myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
             $scope.humidity = data.main.humidity;
             $scope.icon = data.weather[0].icon;
             $scope.icon = "http://openweathermap.org/img/w/" + $scope.icon + ".png";
-        });
+        }).error('error');
     });
+
+    
+
+    $scope.findWeather = function (form) {
+        if($scope.form.$valid) {
+            $scope.position.city = $scope.new.city;
+            $scope.position.country = $scope.new.country;
+
+            $scope.openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+                $scope.position.city + "," + $scope.position.country + "&units=metric&appid=" + $scope.myApiKey;
+
+            console.log($scope.openWeatherURL);
+
+            $http.get($scope.openWeatherURL).success(function(data){
+                $scope.description = data.weather[0].description;
+                $scope.speed = data.wind.speed;
+                // m per s
+                $scope.temp = data.main.temp;
+                $scope.pressure = data.main.pressure;
+                $scope.humidity = data.main.humidity;
+                $scope.icon = data.weather[0].icon;
+                $scope.icon = "http://openweathermap.org/img/w/" + $scope.icon + ".png";
+            });
+        }
+    }
 }]);
+
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9a899e98ac276d14ee3ba6cd2a21598d
